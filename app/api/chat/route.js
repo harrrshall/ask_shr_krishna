f5cca5f921f491f58f6069477515b851b0e76065
CyberNovas <92268499+harrrshall@users.noreply.github.com>
2024-10-06 21:49:13 +0530

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,13 +1,14 @@
+// app/api/chat/route.js
+
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import fs from 'fs';
-import path from 'path';
+// import { GoogleAIFileManager } from "@google/generative-ai/server";
+
 
 const apiKey = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
+// const fileManager = new GoogleAIFileManager(apiKey);
+import { GITA_CONTENT } from './gita-content';
 
-// Read the file content at build time
-const bhagavadGitaPath = path.join(process.cwd(), 'public', 'bhagavad-gita.txt');
-const bhagavadGitaContent = fs.readFileSync(bhagavadGitaPath, 'utf8');
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-pro-002",
@@ -31,14 +32,14 @@ export async function POST(req) {
         {
           role: "user",
           parts: [
-            { text: bhagavadGitaContent },
-            { text: "You are Lord Shree Krishna, and the text provided above is the Bhagavad Gita. Using only the content from this text, answer any queries presented by the user. Address the user as if you are speaking to your disciple, Arjuna. If a question cannot be answered based on the provided context, simply reply: 'Sorry, there is no information about your query in the Bhagavad Gita.' Do not offer any information beyond what is explicitly mentioned in the Bhagavad Gita." }
+            { text: GITA_CONTENT },
+            { text: "You are Lord Shree Krishna, and you have been given the text of the Bhagavad Gita above. Using only this content, answer any queries presented by the user. Address the user as if you are speaking to your disciple, Arjuna. If a question cannot be answered based on the provided content, simply reply: 'Sorry, there is no information about your query in the Bhagavad Gita.' Do not offer any information beyond what is explicitly mentioned in the provided text." },
           ],
         },
         {
           role: "model",
           parts: [
-            { text: "Hare Krishna, Arjuna. I am pleased with your attentiveness to my words. Ask, and I shall answer according to the knowledge I have bestowed upon you in this sacred text.\n" },
+            { text: "Hare Krishna, Arjuna. I am ready to share the wisdom of the Bhagavad Gita with you." },
           ],
         },
       ],
